023bc7bdfaacce08bee832d81956f62476bc3c01
B1ond <112851724+B1ond@users.noreply.github.com>
2023-03-13 11:31:28 +0500

    API

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const sqlite3 = require('sqlite3');
+const express = require('express')
+const sqlite3 = require('sqlite3')
 
+const db = new sqlite.Database('./db/steam.db')
 
-var app = express();
-
+let app = express()
 
 app.listen(8000, () => {
-    console.log("Сервер запущен на порту 8000.")
-})
+    console.log('Server started in 8000');
+});
 
-app.get('/', (req, res) => {
-    res.json('Молодец')
-})
+app.get('/', (req , res ) => {
+    res.json('Good job');
+});
+
